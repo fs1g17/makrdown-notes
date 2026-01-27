f3d389a41fd5bceff1dabded18bd11d82d50efe4
fs1g17 <fs1g17@soton.ac.uk>
2026-01-27 17:02:59 +0400

    test: ensure that the error message is displayed when trying to create note with empty title

--- front/app/(auth)/folders/[[...folderId]]/_components/CreateNoteDialog.test.tsx
+++ front/app/(auth)/folders/[[...folderId]]/_components/CreateNoteDialog.test.tsx
@@ -1,6 +1,7 @@
 import { screen, render } from "@testing-library/react"
 import { CreateNoteDialog } from "./CreateNoteDialog"
 import { queryClient, QueryClientProvider } from "@/lib/react-query-testing";
+import userEvent from "@testing-library/user-event";
 
 describe("Create note dialog", () => {
   it("should display title", async () => {
@@ -16,5 +17,25 @@ describe("Create note dialog", () => {
     );
 
     expect(screen.getByText("Title")).toBeVisible();
+  });
+
+  it("should display validation message if trying to create note with empty title", async () => {
+    render(
+      <QueryClientProvider>
+        <CreateNoteDialog
+          folderId={undefined}
+          folderQueryKey={["folders", { folderId: "root" }]}
+          open={true}
+          onClose={() => { }}
+        />
+      </QueryClientProvider>
+    );
+
+    const createNoteButton = screen.getByText("Create Note");
+    await userEvent.click(createNoteButton);
+
+    const errorMessage = screen.getByRole("alert");
+    expect(errorMessage).toBeVisible();
+    expect(errorMessage).toHaveTextContent("Input a note title");
   });
 })
