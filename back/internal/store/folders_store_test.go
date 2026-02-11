38b994612f2f13469902fb5632b3681177697b93
fs1g17 <fs1g17@soton.ac.uk>
2026-02-11 15:31:57 +0400

    test(folder_store): test UserOwnsFolder

--- back/internal/store/folders_store_test.go
+++ back/internal/store/folders_store_test.go
@@ -1,10 +1,32 @@
 package store
 
 import (
+	"database/sql"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func createRootFolder(t *testing.T, db *sql.DB, folderStore PostgresFoldersStore, user *User) int64 {
+	t.Helper()
+
+	tx, err := db.Begin()
+	if err != nil {
+		t.Fatalf("failed to begin tx: %v", err)
+	}
+
+	rootFolderId, err := folderStore.CreateFolderTx(tx, user.ID, nil, "root")
+	if err != nil {
+		t.Fatalf("failed to create root folder: %v", err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		t.Fatalf("failed to commit tx: %v", err)
+	}
+
+	return rootFolderId
+}
 
 func TestCreateFolder(t *testing.T) {
 	db := SetupTestDB(t)
@@ -18,14 +40,7 @@ func TestCreateFolder(t *testing.T) {
 	var rootFolderId int64
 	subfolderName := "subfolder"
 	t.Run("successfully creates root folder", func(t *testing.T) {
-		tx, err := db.Begin()
-		if err != nil {
-			t.Fatalf("failed to begin tx: %v", err)
-		}
-
-		rootFolderId, err = folderStore.CreateFolderTx(tx, user.ID, nil, "root")
-		assert.NoError(t, err)
-		tx.Commit()
+		rootFolderId = createRootFolder(t, db, *folderStore, user)
 
 		query := `
 		SELECT id, user_id, parent_id, name, created_at, updated_at
@@ -78,14 +93,7 @@ func TestGetRootFolder(t *testing.T) {
 	user, err := createTestUser(t, db, userStore, "Theo", "drumandbassbob@gmail.com", "Password")
 	assert.NoError(t, err)
 
-	tx, err := db.Begin()
-	if err != nil {
-		t.Fatalf("failed to begin tx: %v", err)
-	}
-
-	rootFolderId, err := folderStore.CreateFolderTx(tx, user.ID, nil, "root")
-	assert.NoError(t, err)
-	tx.Commit()
+	rootFolderId := createRootFolder(t, db, *folderStore, user)
 
 	t.Run("gets root folder for existing user", func(t *testing.T) {
 		dbFolderId, err := folderStore.GetRootFolder(user.ID)
@@ -97,5 +105,31 @@ func TestGetRootFolder(t *testing.T) {
 		dbFolderId, err := folderStore.GetRootFolder(72)
 		assert.Error(t, err)
 		assert.Equal(t, int64(0), dbFolderId)
+	})
+}
+
+func TestUserOwnsFolder(t *testing.T) {
+	db := SetupTestDB(t)
+	TruncateTables(t, db)
+	folderStore := NewPostgresFoldersStore(db)
+	userStore := NewPostgresUserStore(db)
+
+	user, err := createTestUser(t, db, userStore, "Theo", "drumandbassbob@gmail.com", "Password")
+	assert.NoError(t, err)
+	user2, err := createTestUser(t, db, userStore, "Theo2", "example@gmail.com", "Password")
+	assert.NoError(t, err)
+
+	rootFolderId := createRootFolder(t, db, *folderStore, user)
+
+	t.Run("returns true when user owns folder", func(t *testing.T) {
+		owns, err := folderStore.UserOwnsFolder(user.ID, rootFolderId)
+		assert.NoError(t, err)
+		assert.True(t, owns)
+	})
+
+	t.Run("returns false when user doesn't own folder", func(t *testing.T) {
+		owns, err := folderStore.UserOwnsFolder(user2.ID, rootFolderId)
+		assert.Error(t, err)
+		assert.False(t, owns)
 	})
 }
